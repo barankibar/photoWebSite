4b83852b864b93109264375e8ae8dde5cfafd59b
barankibar <56038362+barankibar@users.noreply.github.com>
2022-09-09 17:32:04 +0300

    add public dir and middleware

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "temp/index.html"));
+});
 
 app.listen(port, (req, res) => {
   console.log(`Server started on ${port} port`);
